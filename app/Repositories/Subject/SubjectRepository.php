92b0182cd026c27b5e781675c8de7ba38672ae49
MedjadjiAbdelkadir <medjadjiabdelkadir22@gmail.com>
2024-02-25 14:21:57 +0100

    create: create SubjectRepository && EloquentSubject

---
+++ app/Repositories/Subject/SubjectRepository.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Repositories\Subject;
+
+interface SubjectRepository
+{
+    /**
+     * Get all available Subject.
+     * @return mixed
+     */
+    public function all();
+
+    /**
+     * {@inheritdoc}
+     */
+    public function create(array $data);
+
+    /**
+     * {@inheritdoc}
+     */
+    public function update($id, array $data);
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete($id);
+
+    /**
+     * Paginate Subjects.
+     *
+     * @param $perPage
+     * @param null $search
+     * @param null $status
+     * @return mixed
+     */
+    public function paginate($perPage, $search = null, $status = null);
+}
