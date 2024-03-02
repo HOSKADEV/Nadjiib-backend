f3686557fd445544abee8c4bb5e8f19f6410d0a3
MedjadjiAbdelkadir <medjadjiabdelkadir22@gmail.com>
2024-03-02 11:39:50 +0100

    add:add file app.php in ar and en language

---
+++ lang/en/app.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'dashboard' => 'Dashboard',
+    
+    'actions'   => 'Actions',
+    'close'     => 'Close',
+    
+    'create'    => 'Create',
+    'edit'      => 'Edit',
+    'update'    => 'Update',   
+    'delete'    => 'Delete',
+];
