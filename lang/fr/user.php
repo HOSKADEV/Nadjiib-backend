a8cbad86f79c118fd582791699fe7fee61d3304e
MedjadjiAbdelkadir <medjadjiabdelkadir22@gmail.com>
2024-03-02 21:46:10 +0100

    add:add file user.php in ar & en and fr language

---
+++ lang/fr/user.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'users'  => 'utilisateurs',
+    'name'   => 'Nom', 
+    'email'  => 'Email', 
+    'phone'  => 'Téléphone', 
+    'gender' => 'Gender',
+    'image'  => 'Image', 
+    'role'   => 'Rôle',
+    'status' => 'Statut',
+
+    'delete_confirmation' => 'Êtes-vous sûr de vouloir supprimer cet utilisateur ?',
+    'label'=> [
+        'status'   => 'statut',
+        'role'     => 'rôle',
+        'search'   => 'recherche',
+    ],
+    'placeholder' =>[
+        'search' => "Tapez le nom d'utilisateur, l'e-mail ou le numéro de téléphone"
+    ],
+    'select'=>[
+        'status' => "Choisir le statut d'utilisateur",
+        'role'   => "Choisissez le rôle d'utilisateur",
+    ], 
+    "statuss" => [
+        'active'   => 'Activé',
+        'inactive' => 'Pas activé',
+    ], 
+];
