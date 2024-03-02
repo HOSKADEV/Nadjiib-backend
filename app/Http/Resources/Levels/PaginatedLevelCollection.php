ddccc2cb2454d5d735f97ff6f15998330cf52c5d
Adda Benkoceir Mohamed <64089992+addabenkoceir13@users.noreply.github.com>
2024-03-02 11:18:14 +0100

    create: create controller api and Resource Level

---
+++ app/Http/Resources/Levels/PaginatedLevelCollection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\Levels;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class PaginatedLevelCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+          'level' => $this->collection,
+          'meta' => [
+            'current_page' => $this->currentPage(),
+            'last_page' => $this->lastPage(),
+            'per_page' => $this->perPage(),
+            'total' => $this->total(),
+            'count' => $this->count(),
+          ]
+        ];
+    }
+}
