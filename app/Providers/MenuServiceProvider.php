9cd5df8445ddfbe674cab9d89380adf6da13dc8a
MedjadjiAbdelkadir <medjadjiabdelkadir22@gmail.com>
2024-03-02 14:56:17 +0100

    update:update in MenuServiceProvider.php

--- app/Providers/MenuServiceProvider.php
+++ app/Providers/MenuServiceProvider.php
@@ -6,27 +6,27 @@ use Illuminate\Support\ServiceProvider;
 
 class MenuServiceProvider extends ServiceProvider
 {
-  /**
-   * Register services.
-   *
-   * @return void
-   */
-  public function register()
-  {
-    //
-  }
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 
-  /**
-   * Bootstrap services.
-   *
-   * @return void
-   */
-  public function boot()
-  {
-    $verticalMenuJson = file_get_contents(base_path('resources/menu/verticalMenu.json'));
-    $verticalMenuData = json_decode($verticalMenuJson);
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $verticalMenuJson = file_get_contents(base_path('resources/menu/verticalMenu.json'));
+        $verticalMenuData = json_decode($verticalMenuJson);
 
-    // Share all menuData to all the views
-    \View::share('menuData', [$verticalMenuData]);
-  }
+        // Share all menuData to all the views
+        \View::share('menuData', [$verticalMenuData]);
+    }
 }
