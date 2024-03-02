aa924fe59eb82f61ef50c7979f6d5486d53d56cf
MedjadjiAbdelkadir <medjadjiabdelkadir22@gmail.com>
2024-03-02 11:33:07 +0100

    add:add file subject.php in ar and en language

---
+++ lang/en/subject.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'dashboard' => 'Dashboard',
+    'subjects'  => 'Subjects',
+    'name_ar'   => 'Name AR',
+    'name_en'   => 'Name EN',
+    'name_fr'   => 'Name FR',
+    'image'     => 'Image',
+
+    'create'    => 'Create Subject',
+    'edit'      => 'Edit Subject',
+    'update'    => 'Update Subject',   
+    'delete'    => 'Delete Subject', 
+    'delete_confirmation' => 'Are you sure you want to delete this subject ?',
+
+    'label'=> [
+        'name'     => 'Name',
+        'name_ar'  => 'Name of Subject in Arabic',
+        'name_en'  => 'Name of Subject in English',
+        'name_fr'  => 'Name of Subject in French',
+    ],
+    'placeholder' =>[
+        'name'    => 'Write the name of the Subject',
+        'name_ar' => 'Write the name of the Subject in Arabic',
+        'name_en' => 'Write the name of the Subject in English',
+        'name_fr' => 'Write the name of the Subject in French'
+    ]
+];
