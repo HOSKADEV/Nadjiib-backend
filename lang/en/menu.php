c3f593a1b30855867b65c4de93c0664ece2450f0
MedjadjiAbdelkadir <medjadjiabdelkadir22@gmail.com>
2024-03-02 11:40:31 +0100

    add:add file menu.php in ar and en language

---
+++ lang/en/menu.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'dashboard'  => 'Dashboard',
+    'pages'      => 'Pages', 
+    'sections'   => 'Sections',
+    'levels'     => 'Levels',
+    'subjects'   => 'Subjects',
+    'level-subjects' => 'Subjects Level',
+    'coupons'        => 'Coupons' 
+];
