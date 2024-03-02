2ad10673ed63dd7e3d4d695474549f789a453901
MedjadjiAbdelkadir <medjadjiabdelkadir22@gmail.com>
2024-03-02 11:54:30 +0100

    update:bind files Repository & Eloquent in EloquentRepositoryProvider.php

--- app/Providers/EloquentRepositoryProvider.php
+++ app/Providers/EloquentRepositoryProvider.php
@@ -4,11 +4,15 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\Level\EloquentLevel;
+use App\Repositories\Coupon\EloquentCoupon;
 use App\Repositories\Level\LevelRepository;
+use App\Repositories\Coupon\CouponRepository;
 use App\Repositories\Section\EloquentSection;
 use App\Repositories\Subject\EloquentSubject;
 use App\Repositories\Section\SectionRepository;
 use App\Repositories\Subject\SubjectRepository;
+use App\Repositories\LevelSubject\EloquentLevelSubject;
+use App\Repositories\LevelSubject\LevelSubjectRepository;
 
 class EloquentRepositoryProvider extends ServiceProvider
 {
@@ -22,6 +26,9 @@ class EloquentRepositoryProvider extends ServiceProvider
         $this->app->bind(SectionRepository::class, EloquentSection::class);
         $this->app->bind(SubjectRepository::class, EloquentSubject::class);
         $this->app->bind(LevelRepository::class, EloquentLevel::class);
+        $this->app->bind(LevelSubjectRepository::class, EloquentLevelSubject::class);
+
+        $this->app->bind(CouponRepository::class, EloquentCoupon::class);
     }
 
     /**
