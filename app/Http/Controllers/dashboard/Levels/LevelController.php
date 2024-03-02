0dfbc86b267ff5c69777cb06421c302fab8bd9fa
MedjadjiAbdelkadir <medjadjiabdelkadir22@gmail.com>
2024-03-02 11:56:40 +0100

    update:update in LevelController

--- app/Http/Controllers/dashboard/Levels/LevelController.php
+++ app/Http/Controllers/dashboard/Levels/LevelController.php
@@ -2,19 +2,37 @@
 
 namespace App\Http\Controllers\Dashboard\Levels;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\Level\LevelRepository;
+use App\Repositories\Section\SectionRepository;
 
 class LevelController extends Controller
 {
+    private $sections;
+    private $levels;
+
+    /**
+     * LevelController constructor.
+     * @param SectionRepository $sections
+     * @param LevelRepository $levels
+     */
+    public function __construct(SectionRepository $sections , LevelRepository $levels)
+    {
+        $this->sections = $sections;
+        $this->levels = $levels;
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
-     */
-    public function index()
+     */ 
+    public function index(Request $request)
     {
-        //
+        $sections = $this->sections->all();
+        $levels = $this->levels->paginate($perPage = 10, $request->search);
+        return view('dashboard.level.index',compact('levels','sections'));
     }
 
     /**
@@ -35,7 +53,9 @@ class LevelController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->levels->create($request->all());
+        toastr()->success(trans('message.success.create'));
+        return redirect()->route('dashboard.levels.index');
     }
 
     /**
@@ -67,9 +87,11 @@ class LevelController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        //
+        $this->levels->update($request->id,$request->all());
+        toastr()->success(trans('message.success.update'));
+        return redirect()->route('dashboard.levels.index');
     }
 
     /**
@@ -78,8 +100,10 @@ class LevelController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request)
     {
-        //
+        $this->levels->delete($request->id);
+        toastr()->success(trans('message.success.delete'));
+        return redirect()->route('dashboard.levels.index');
     }
 }
