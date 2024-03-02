540c20218deba7a76ed81dd55cabf4fbad9e9227
MedjadjiAbdelkadir <medjadjiabdelkadir22@gmail.com>
2024-03-02 11:58:05 +0100

    update:update in SectionController Class

--- app/Http/Controllers/dashboard/Section/SectionController.php
+++ app/Http/Controllers/dashboard/Section/SectionController.php
@@ -2,19 +2,31 @@
 
 namespace App\Http\Controllers\Dashboard\Section;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\Section\SectionRepository;
 
 class SectionController extends Controller
 {
+    private $sections;
+
+    /**
+     * SectionController constructor.
+     * @param SectionRepository $sections
+     */
+    public function __construct(SectionRepository $sections)
+    {
+        $this->sections = $sections;
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $sections = $this->sections->paginate($perPage = 10, $request->search);
+        return view('dashboard.section.index',compact('sections'));
     }
 
     /**
@@ -35,7 +47,9 @@ class SectionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->sections->create($request->all());
+        toastr()->success(trans('message.success.create'));
+        return redirect()->route('dashboard.sections.index');
     }
 
     /**
@@ -67,9 +81,11 @@ class SectionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        //
+        $this->sections->update($request->id,$request->all());
+        toastr()->success(trans('message.success.update'));
+        return redirect()->route('dashboard.sections.index');
     }
 
     /**
@@ -78,8 +94,10 @@ class SectionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request)
     {
-        //
+        $this->sections->delete($request->id);
+        toastr()->success(trans('message.success.delete'));
+        return redirect()->route('dashboard.sections.index');
     }
 }
