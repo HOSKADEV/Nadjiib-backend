27b6b6aefd723aa685b767f65097e047b75b348a
Adda Benkoceir Mohamed <64089992+addabenkoceir13@users.noreply.github.com>
2024-03-02 11:17:41 +0100

    create: create controller api and Resource Subject

--- app/Http/Resources/Subject/SubjectResource.php
+++ app/Http/Resources/Subject/SubjectResource.php
@@ -14,6 +14,14 @@ class SubjectResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+          'id'      => $this->id ,
+          'name_ar' => $this->name_ar ,
+          'name_fr' => $this->name_fr ,
+          'name_en' => $this->name_en ,
+          'image'   => is_null($this->image) ? null : $this->image,
+          'type'    => $this->type ,
+          'created' => $this->created_at->format('d-m-Y') ,
+        ];
     }
 }
