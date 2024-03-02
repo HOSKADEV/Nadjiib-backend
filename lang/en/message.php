b8d1c5cf3445fa82696e82246f972a391d0a5897
MedjadjiAbdelkadir <medjadjiabdelkadir22@gmail.com>
2024-03-02 11:33:46 +0100

    add:add file message.php in ar and en language

---
+++ lang/en/message.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'success'    => [
+        'create' => 'Data has been saved successfully',
+        'update' => 'The data has been updated successfully',
+        'delete' => 'The data has been deleted successfully',
+    ],
+    'error'    => [
+        'create' => 'An error occurred saving data',
+        'update' => 'An error occurred updating data',
+        'delete' => 'An error occurred deleting data',
+    ],
+];
