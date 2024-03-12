7df01b00285dfe313ebdf27b74a9ebb7c136da76
Adda Benkoceir Mohamed <64089992+addabenkoceir13@users.noreply.github.com>
2024-03-12 17:14:53 +0100

    create or update in the copone

--- app/Http/Controllers/API/Coupons/CouponController.php
+++ app/Http/Controllers/API/Coupons/CouponController.php
@@ -3,9 +3,34 @@
 namespace App\Http\Controllers\API\Coupons;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Coupons\PaginateCouponCollection;
+use App\Repositories\Coupon\CouponRepository;
 use Illuminate\Http\Request;
-
+use Exception;
 class CouponController extends Controller
 {
-    //
+    private $coupon;
+
+    public function __construct(CouponRepository $coupon){
+      $this->coupon = $coupon;
+    }
+
+    public function getCoupon(){
+      try
+      {
+          $coupon = $this->coupon->paginate(10);
+
+          return response()->json([
+            'status' => true,
+            'coupon' => new PaginateCouponCollection($coupon)
+          ]);
+      }
+      catch(Exception $e)
+      {
+        return response()->json([
+          'status'  => 0,
+          'message' => $e->getMessage()
+        ]);
+      }
+    }
 }
