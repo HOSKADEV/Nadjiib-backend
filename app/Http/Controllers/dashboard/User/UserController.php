13c05005db4ae311bbb0d726f621c6bf28b5add0
MedjadjiAbdelkadir <medjadjiabdelkadir22@gmail.com>
2024-03-13 23:26:48 +0100

    update in UserController Dashboard

--- app/Http/Controllers/dashboard/User/UserController.php
+++ app/Http/Controllers/dashboard/User/UserController.php
@@ -2,22 +2,34 @@
 
 namespace App\Http\Controllers\Dashboard\User;
 
+use App\Enums\CouponType;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Repositories\User\UserRepository;
+use App\Repositories\Coupon\CouponRepository;
+use App\Repositories\Teacher\TeacherRepository;
+use App\Http\Requests\User\UpgradeAccountRequest;
 
 class UserController extends Controller
 {
     private $users;
+    private $teachers;
+    private $coupons;
 
     /**
      * UserController constructor.
      * @param UserRepository $users
+     * @param TeacherRepository $teachers
+     * @param CouponRepository $coupons
      */
-    public function __construct(UserRepository $users)
+    public function __construct(UserRepository $users, TeacherRepository $teachers, CouponRepository $coupons)
     {
         $this->users = $users;
+        $this->teachers = $teachers;
+        $this->coupons = $coupons;
     }
+
     /**
      * Display a listing of the resource.
      *
@@ -26,7 +38,8 @@ class UserController extends Controller
     public function index(Request $request)
     {
         $users = $this->users->paginate($perPage = 10, $request->search, $request->status);
-        return view('dashboard.user.index',compact('users'));
+
+        return view('dashboard.user.index', compact('users'));
     }
 
     /**
@@ -69,7 +82,8 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = $this->users->find($id);
+        return view('dashboard.user.edit', compact('user'));
     }
 
     /**
@@ -81,7 +95,9 @@ class UserController extends Controller
      */
     public function update(Request $request)
     {
-        //
+        $this->users->update($request->id, $request->all());
+        toastr()->success(trans('message.success.update'));
+        return redirect()->route('dashboard.users.index');
     }
 
     /**
@@ -92,6 +108,44 @@ class UserController extends Controller
      */
     public function destroy(Request $request)
     {
-        //
+        $this->users->delete($request->id);
+        toastr()->success(trans('message.success.delete'));
+        return redirect()->route('dashboard.users.index');
+    }
+
+    /**
+     * Update Account Status.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function changeStatus(Request $request)
+    {
+        $this->users->changeStatus($request->id);
+        toastr()->success(trans('message.success.update'));
+        return redirect()->route('dashboard.users.index');
+    }
+
+    /**
+     * Upgrade Account .
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function upgradeAccount(UpgradeAccountRequest $request)
+    {
+        // $this->coupons->create();
+        $data = [
+            'type' => CouponType::INVITATION,
+            'code' => Str::upper(Str::random(8))
+        ];
+        $coupon = $this->coupons->create($data);
+
+        $this->teachers->create([
+            'user_id'  => $request->id,
+            'coupon_id' => $coupon->id
+        ]);
+        toastr()->success(trans('message.success.upgrade'));
+        return redirect()->route('dashboard.users.index');
     }
 }
