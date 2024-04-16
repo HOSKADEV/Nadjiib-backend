d4890814dab37da9869c1afc4038074ee31f92be
hoskamohamed <addamohamed@hoskadev.com>
2024-04-16 23:31:34 +0100

    create a new review repositorey

---
+++ app/Repositories/Review/EloquentReview.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Repositories\Review;
+
+use App\Models\Review;
+use App\Repositories\Review\ReviewRepository;
+
+class EloquentReview implements ReviewRepository
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function all(){
+        return Review::all();
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function find($id)
+    {
+        return Review::find($id);
+    }
+
+    /**
+     * @param array $data
+     * @return mixed
+     */
+    public function create(array $data)
+    {
+        $review = Review::create($data);
+
+        return $review;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function update($id, array $data)
+    {
+        $review = $this->find($id);
+
+        $review->update($data);
+
+        return $review;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete($id)
+    {
+        $review = $this->find($id);
+
+        return $review->delete();
+    }
+
+    /**
+     * @param $perPage
+     * @param null $status
+     * @param null $searchFrom
+     * @param $searchTo
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator|mixed
+     */
+    public function paginate($perPage, $courseId)
+    {
+
+        $review = Review::whereCourseId($courseId)->orderBy('id', 'desc')
+                    ->paginate($perPage);
+
+        return $review;
+    }
+}
