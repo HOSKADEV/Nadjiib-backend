04fb45b609043fdcf117271d271afec23a78741a
hoskamohamed <addamohamed@hoskadev.com>
2024-04-16 23:37:33 +0100

    create new controller widhlist

---
+++ app/Http/Controllers/API/Wishlist/WishlistController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\API\Wishlist;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WishlistController extends Controller
+{
+    //
+}
