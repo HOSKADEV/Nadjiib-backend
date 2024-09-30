9c3cbbede4203c184f37740e0f99a5b660f4263d
AbdelaliKDM <khademallah.abdelali@gmail.com>
2024-09-30 14:32:48 +0100

    fix teachers get

--- app/Http/Controllers/Dashboard/Payment/PaymentController.php
+++ app/Http/Controllers/Dashboard/Payment/PaymentController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Dashboard\Payment;
 
-use App\Http\Controllers\Controller;
+use App\Models\Payment;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PaymentController extends Controller
 {
-    //
+    public function index(Request $request){
+
+      if($request->has('date'));
+      $payments = Payment::all();
+      dd($payments);
+    }
 }
